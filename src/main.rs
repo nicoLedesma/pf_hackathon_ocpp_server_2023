60224d390fe6e0bd55dd77ad950ce4cdd76384a4
Nicolas Ledesma <nicoledesma25@gmail.com>
2023-03-15 13:42:48 -0300

    [chatgpt] chatgpt generated initial server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use tokio_native_tls::{TlsAcceptor, TlsStream};
+use tokio_tungstenite::accept_async;
+use tungstenite::handshake::server::{Request, Response};
+use tungstenite::Message;
+
+#[tokio::main]
+async fn main() {
+    // Load the TLS certificate and private key
+    let cert = tokio::fs::read("cert.pem").await.unwrap();
+    let key = tokio::fs::read("key.pem").await.unwrap();
+    let identity = tokio_native_tls::Identity::from_pem(cert.as_ref(), key.as_ref());
+
+    // Create the TLS acceptor
+    let tls_acceptor = TlsAcceptor::from(identity);
+
+    // Bind the TCP listener
+    let addr = "0.0.0.0:8080".parse::<SocketAddr>().unwrap();
+    let tcp_listener = TcpListener::bind(&addr).await.unwrap();
+
+    println!("Listening on: {}", addr);
+
+    // Accept incoming connections
+    loop {
+        let (tcp_stream, _) = tcp_listener.accept().await.unwrap();
+        let tls_stream = tls_acceptor.accept(tcp_stream).await.unwrap();
+        tokio::spawn(handle_connection(tls_stream));
+    }
+}
+
+async fn handle_connection(tls_stream: TlsStream<TcpStream>) {
+    // Accept the WebSocket handshake
+    let ws_stream = accept_async(tls_stream).await.unwrap();
+
+    // Handle incoming messages
+    for msg in ws_stream.try_iter() {
+        match msg {
+            Ok(Message::Text(text)) => {
+                println!("Received text message: {}", text);
+            }
+            Ok(Message::Binary(data)) => {
+                println!("Received binary message with length: {}", data.len());
+            }
+            Ok(Message::Ping(data)) => {
+                ws_stream.send(Message::Pong(data)).await.unwrap();
+            }
+            Ok(Message::Pong(_)) => {}
+            Ok(Message::Close(_)) => {
+                break;
+            }
+            Err(err) => {
+                eprintln!("Error receiving message: {:?}", err);
+                break;
+            }
+        }
+    }
 }
