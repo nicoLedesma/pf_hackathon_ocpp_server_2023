799d6298501ef17de169bfc5d1c5816440659ca6
Hugo O. Rivera <hugo@roguh.com>
2023-03-15 16:58:39 -0700

    Improve logging and error recovery

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ use tungstenite::Message;
 async fn main() {
     // Load the TLS certificate and private key
     let server_identity_pkcs12_der = tokio::fs::read("identity.p12.der").await.unwrap();
-    let key = tokio::fs::read_to_string("identity_password.txt").await.unwrap();
+    let key = tokio::fs::read_to_string("identity_password.txt")
+        .await
+        .unwrap();
     let pkcs12 = openssl::pkcs12::Pkcs12::from_der(&server_identity_pkcs12_der).unwrap();
     let identity =
         tokio_native_tls::native_tls::Identity::from_pkcs12(&pkcs12.to_der().unwrap(), &key)
@@ -21,17 +23,28 @@ async fn main() {
     );
 
     // Bind the TCP listener
-    let addr = "0.0.0.0:8765".parse::<SocketAddr>().unwrap();
+    let addr = "127.0.0.1:8765".parse::<SocketAddr>().unwrap();
     let tcp_listener = TcpListener::bind(&addr).await.unwrap();
 
     println!("Listening on: {}", addr);
 
     // Accept incoming connections
     loop {
-        let (tcp_stream, _) = tcp_listener.accept().await.unwrap();
-        let tls_stream = tls_acceptor.accept(tcp_stream).await.unwrap();
-        tokio::spawn(handle_connection(tls_stream));
+        if let Err(e) = accept_connection(&tcp_listener, &tls_acceptor).await {
+            println!("Error! {:?}", e);
+        }
     }
+}
+
+async fn accept_connection(
+    tcp_listener: &TcpListener,
+    tls_acceptor: &tokio_native_tls::TlsAcceptor,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let (tcp_stream, _) = tcp_listener.accept().await?;
+    println!("Connection received from {:?}", tcp_stream.peer_addr());
+    let tls_stream = tls_acceptor.accept(tcp_stream).await?;
+    tokio::spawn(handle_connection(tls_stream));
+    Ok(())
 }
 
 async fn handle_connection(tls_stream: TlsStream<TcpStream>) {
